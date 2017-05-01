33ea60f5959a419a2bea1c8de024d9725a09e608
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2017-05-01 20:47:26 +0100

    websockets

--- pirate/index.js
+++ pirate/index.js
@@ -1,11 +1,19 @@
 "use strict";
 let phatbeat = require('phatbeat');
+let WebSocket = require('ws');
 let buttons = phatbeat.getButtonPins();
-buttons.forEach(function(button) {
-    const stream = phatbeat.buttonStream(button['pin']);
-    stream.on("pinChange", function(pin, pinState) {
-        if(pinState == 1) {
-            console.log(button['name']);
-        }
+const ws = new WebSocket('ws://' + location.hostname + ':8000');
+ws.on('open', function(ws) {
+    buttons.forEach(function(button) {
+        const stream = phatbeat.buttonStream(button['pin']);
+        stream.on("pinChange", function(pin, pinState) {
+            if(pinState == 1) {
+                ws.send(button["name"]);
+                console.log(button['name']);
+            }
+        });
     });
+};)
+ws.addEventListener('open', function (evt) {
+    console.log('connected');
 });
