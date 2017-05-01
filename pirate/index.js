6e6414b128fadd4de1c87e730508b213159df069
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2017-05-01 20:35:48 +0100

    Couple of silly mistakes

--- pirate/index.js
+++ pirate/index.js
@@ -1,7 +1,8 @@
+"use strict";
 let phatbeat = require('phatbeat');
 let buttons = phatbeat.getButtonPins();
 buttons.forEach(function(button) {
-    const stream = phatBeat.ButtonStream(button['pin']);
+    const stream = phatbeat.buttonStream(button['pin']);
     stream.on("pinChange", function(pin, pinState) {
         if(pinState == 1) {
             console.log(button['name']);
